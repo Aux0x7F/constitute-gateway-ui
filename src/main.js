c97c6cd19138f7820434d04bf004d171621ba6f4
Aux <aux_0x7F@proton.me>
2026-05-17 10:30:54 -0700

    feat: consume shared runtime shell posture

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ import { RUNTIME_DIAGNOSTIC_OPERATOR_PLANES, attachRuntimeDiagnostics } from "..
 import {
   browserStorageShellContext,
   deriveRuntimeShellState,
-} from "../../constitute-account/runtime-shell-state.js";
+} from "../../constitute-ui/src/runtime-shell-state.js";
 import {
   gatewayRuntimeClientModule,
   gatewaySurfaceAttachContext,
